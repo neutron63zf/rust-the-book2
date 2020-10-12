b6bb11541ffb9d53113f97ca31111d6e7cfc1361
Nkowne63 <neutron63zf@gmail.com>
2020-10-13 00:14:06 +0900

    add: examples

--- ch-03/variables/src/main.rs
+++ ch-03/variables/src/main.rs
@@ -8,4 +8,78 @@ fn main() {
     let x = x * 2;
 
     println!("The value of x is: {}", x);
+
+    // let guess: u32 = "42".parse().expect("Not a number!");
+
+    // let x = 2.0; // f64
+
+    // let y: f32 = 3.0; // f32
+
+    // 足し算
+    // let sum = 5 + 10;
+
+    // 引き算
+    // let difference = 95.5 - 4.3;
+
+    // 掛け算
+    // let product = 4 * 30;
+
+    // 割り算
+    // let quotient = 56.7 / 32.2;
+
+    // 余り
+    // let remainder = 43 % 5;
+
+    // let t = true;
+
+    // let f: bool = false; // 明示的型注釈付きで
+
+    // let c = 'z';
+    // let z = 'ℤ';
+    // let heart_eyed_cat = '😻';
+
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // let tup = (500, 6.4, 1);
+
+    // let (x, y, z) = tup;
+
+    // println!("The value of y is: {}", y);
+
+    // let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    // let five_hundred = x.0;
+
+    // let six_point_four = x.1;
+
+    // let one = x.2;
+
+    // let a = [1, 2, 3, 4, 5];
+
+    // let months = [
+    //     "January",
+    //     "February",
+    //     "March",
+    //     "April",
+    //     "May",
+    //     "June",
+    //     "July",
+    //     "August",
+    //     "September",
+    //     "October",
+    //     "November",
+    //     "December",
+    // ];
+
+    // let a = [1, 2, 3, 4, 5];
+
+    // let first = a[0];
+    // let second = a[1];
+
+    // let a = [1, 2, 3, 4, 5];
+    // let index = 10;
+
+    // let element = a[index];
+
+    // println!("The value of element is: {}", element);
 }
