e7a77042e54d678812d310e152c4986ad695d320
Nkowne63 <neutron63zf@gmail.com>
2020-10-13 00:05:06 +0900

    update: shadowing

--- ch-03/variables/src/main.rs
+++ ch-03/variables/src/main.rs
@@ -1,8 +1,11 @@
 const MAX_POINTS: u32 = 100_000;
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
     println!("The value of x is: {}", x);
 }
