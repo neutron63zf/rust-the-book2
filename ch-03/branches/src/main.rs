50f28ceb92e1abe6ec2c2b5d1543d039e7f04111
Nkowne63 <neutron63zf@gmail.com>
2020-10-19 23:26:26 +0900

    add: else if

--- ch-03/branches/src/main.rs
+++ ch-03/branches/src/main.rs
@@ -2,12 +2,19 @@
 #![allow(unused_variables)]
 
 fn main() {
-    let number = 3;
-    let number = 7;
+    let number = 6;
 
-    if number < 5 {
-        println!("condition was true"); // 条件は真でした
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
     } else {
-        println!("condition was false"); // 条件は偽でした
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
