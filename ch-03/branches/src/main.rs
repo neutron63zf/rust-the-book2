bc616278b69426be1c9cfebec8ec035d19d9e070
Nkowne63 <neutron63zf@gmail.com>
2020-10-19 23:26:14 +0900

    add: if

---
+++ ch-03/branches/src/main.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+fn main() {
+    let number = 3;
+    let number = 7;
+
+    if number < 5 {
+        println!("condition was true"); // 条件は真でした
+    } else {
+        println!("condition was false"); // 条件は偽でした
+    }
+}
